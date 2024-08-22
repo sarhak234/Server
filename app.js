844b69e6f5ac163213f17a73ec0d867fb38554a2
sarthak mehta <sarthakmehta768@gmail.com>
2024-08-22 20:13:45 +0530

    server site 2nd commit

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express=require('express')
 const app=express();
+const cors=require('cors')
+
+app.use(cors())
 
 app.post('/',(req,res)=>{
     res.send('my name is sarthak mehta')
+})
+
+app.listen(5000,()=>{
+    console.log('app is running on port 5000')
 })
